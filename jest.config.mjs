e1dd3730684de6f08cba7ab9a95d64cc452cf090
Hackmann <hackmann657@gmail.com>
2025-01-05 22:04:33 -0300

    Add testes automatizados

---
+++ jest.config.mjs
@@ -0,0 +1,8 @@
+export default {
+  testEnvironment: 'node',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  testPathIgnorePatterns: ['/node_modules/'],
+  verbose: true,
+  transform: {},
+};
